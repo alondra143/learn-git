bbc9d1340a31c09147e06f517ebb802c0c36fd44
alondra 3 <not given>
2021-08-07 12:57:58 -0700

    added js content

---
+++ testing.js
@@ -0,0 +1,9 @@
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello
+
+const itWorked = document.createElement('h2');
+itWorked.innerText = "SUCCESS. NOT A FAILURE.";
+itWorked.setAttribute("id", "attempt");
+button.append(itWorked);
